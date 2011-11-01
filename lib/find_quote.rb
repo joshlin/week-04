09997b30cfd1f4a951af80e3193842bf6540d047
Josh Lin <joshlin@uw.edu>
2011-10-31 23:18:11 -0700

    Add two methods

--- lib/find_quote.rb
+++ lib/find_quote.rb
@@ -7,3 +7,17 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+
+def find_quote(filename, line=nil)
+  if File.exists? filename
+    quotes = all_quotes(filename)
+
+    if line.nil? || line.to_i > quotes.size
+	quotes[rand(quotes.size)]
+    else 
+    	quotes[line]
+    end
+  else
+   "Could not find a quote at this time" 
+  end
+end
